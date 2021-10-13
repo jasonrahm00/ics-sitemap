3b80858550cf8aa1b351ec0a4facb9e63103308c
uspojrahm <jason.rahm@blount.com>
2021-10-13 09:45:46 -0600

    cardData file created

---
+++ src/components/page-objects/card/cardData.js
@@ -0,0 +1,25 @@
+export const icsCards = [
+  {
+    group: 'Power Saws',
+    cards: [
+      {
+        header: 'Neumatic',
+        destination: 'Product Page',
+        link: '/pdp',
+        productList: ['701A Pneumatic Power Cutter']
+      },
+      {
+        header: 'Gas',
+        destination: 'Product Listing page',
+        link: '/plp',
+        productList: ['695XL PG Gas Power Cutter', '695XL Gas Power Cutter', '680ES PG Gas Power Cutter', '680ES Gas Power Cutter']
+      },
+      {
+        header: 'Hydraulic',
+        destination: 'Product Listing Page',
+        link: '/plp',
+        productList: ['890PG Hydraulic Power Cutter', '890F4 Flush Cut Hydraulic Power Cutter', '890F4 Flush Cut Hydraulic Power Cutter']
+      }
+    ]
+  }
+];

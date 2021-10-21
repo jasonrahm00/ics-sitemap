e5fb4a41e2c74e365a90694f2a49bae440692b68
uspojrahm <jason.rahm@blount.com>
2021-10-21 11:46:57 -0600

    black bar footer updated

--- src/components/base/footer/footer.js
+++ src/components/base/footer/footer.js
@@ -7,32 +7,37 @@ import footerNavData from './footerNavData';
 
 function Footer() {
   return (
-    <footer>
-      <nav className="content-wrapper">
-        {footerNavData.map((column, index) => {
-          return (
-            <div key={index}>
-              {column.sections.map((section, secIndex) => {
-                return (
-                  <section key={secIndex}>
-                    <h2>{section.sectionHeader}</h2>
-                    <List component="ul" disablePadding={true} className={section.sectionHeader === 'Follow Us' ? 'social-links' : ''}>
-                      {section.sectionLinks.map((link, linkIndex) => {
-                        return (
-                          <ListItem key={linkIndex} disablePadding={true}>
-                            {link} 
-                          </ListItem>
-                        )
-                      })}
-                    </List>
-                  </section>
-                )
-              })}
-            </div>
-          );
-        })}
-      </nav>
-    </footer>
+    <>
+      <footer>
+        <nav className="content-wrapper">
+          {footerNavData.map((column, index) => {
+            return (
+              <div key={index}>
+                {column.sections.map((section, secIndex) => {
+                  return (
+                    <section key={secIndex}>
+                      <h2>{section.sectionHeader}</h2>
+                      <List component="ul" disablePadding={true} className={section.sectionHeader === 'Follow Us' ? 'social-links' : ''}>
+                        {section.sectionLinks.map((link, linkIndex) => {
+                          return (
+                            <ListItem key={linkIndex} disablePadding={true}>
+                              {link} 
+                            </ListItem>
+                          )
+                        })}
+                      </List>
+                    </section>
+                  )
+                })}
+              </div>
+            );
+          })}
+        </nav>
+      </footer>
+      <div className="black-bar-footer">
+        <p className="content-wrapper">&copy; {new Date().getFullYear()} Jason Rahm</p>
+      </div>
+    </>
   );
 }
 

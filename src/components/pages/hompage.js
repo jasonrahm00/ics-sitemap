7f8e19fe5216470294cb2811c8a1a8daf4e82c5d
uspojrahm <jason.rahm@blount.com>
2021-11-22 16:07:14 -0700

    user segmentation component added to homepage

--- src/components/pages/hompage.js
+++ src/components/pages/hompage.js
@@ -1,13 +1,49 @@
-function Homepage({...props}) {
+import ImgTextOverlay from "../cms-components/image-text-overlay/imgTextOverlay";
+import UserSegmentationWrapper from "../cms-components/user-segmentation/userSegmentationWrapper";
+import UserSegmentationCard from "../cms-components/user-segmentation/userSegmentationCard";
+import TileCollection from "../cms-components/tile-collection/tileCollection";
+import TileComponent from "../cms-components/tile-collection/tile/tileComponent";
+import { PlaceholderText } from "../../utility/variables";
+import PlaceholderImage from '../../media/placeholder-image-large.png';
+
+function Homepage() {
   return (
     <>
-      {props.components && props.components.map((component, index) => {
-        return (
-          <div key={index}>
-            {component}
-          </div>
-        )
-      })}
+      <ImgTextOverlay 
+        header={'ICS Diamond Tools'} 
+        headerTag="h1"
+        subheaderTag="h2"
+        subheader="Don’t Just Cut. Conquer"
+        text={<p>As the world leader and pioneering force in Diamond Chain Technology, ICS concrete chainsaws and diamond chains have revolutionized concrete cutting in the construction industry.  At ICS, we know you don’t have time for tools that are just “good enough”. Your tools had better not just show up, but measure up. Like you, we believe there are no substitutes for hard work and absolutely no shortcuts to getting the job done right. All our products are designed, engineered and manufactured for you to outwork your competition, outlast the toughest jobs, and outperform any challenge.  Your unfair advantage is here, and it’s brought to you by ICS.</p>} 
+      />
+
+      <div className="padding-top">
+        <UserSegmentationWrapper cards={
+          [
+            <UserSegmentationCard 
+              header='ICS Brand Products'
+              image={PlaceholderImage}
+              bodyText={PlaceholderText}
+              ctaURL='/products/ics'
+              ctaText='ICS Brand Page'
+            />,
+            <UserSegmentationCard 
+              header='Merit Brand Products'
+              image={PlaceholderImage}
+              bodyText={PlaceholderText}
+              ctaURL='/products/merit'
+              ctaText='Merit Brand Page'
+            />,
+            <UserSegmentationCard 
+              header='Pentruder Brand Products'
+              image={PlaceholderImage}
+              bodyText={PlaceholderText}
+              ctaURL='/products/pentruder'
+              ctaText='Pentruder Brand Page'
+            />,
+          ]
+        }/>
+      </div>
       <ul className="content-wrapper">
         <li>Homepage
           <ul>
--- src/data/route-data/baseRoutes.js
+++ src/data/route-data/baseRoutes.js
@@ -7,14 +7,7 @@ import { RouteObject } from '../../utility/constructors';
 import ImgTextOverlay from '../../components/cms-components/image-text-overlay/imgTextOverlay';
 
 const BaseRoutes = [
-  new RouteObject('/', () => <Homepage components={[
-    <ImgTextOverlay 
-      header={'ICS Diamond Tools'} 
-      headerTag="h1"
-      subheaderTag="h2"
-      subheader="Don’t Just Cut. Conquer"
-      text={<p>As the world leader and pioneering force in Diamond Chain Technology, ICS concrete chainsaws and diamond chains have revolutionized concrete cutting in the construction industry.  At ICS, we know you don’t have time for tools that are just “good enough”. Your tools had better not just show up, but measure up. Like you, we believe there are no substitutes for hard work and absolutely no shortcuts to getting the job done right. All our products are designed, engineered and manufactured for you to outwork your competition, outlast the toughest jobs, and outperform any challenge.  Your unfair advantage is here, and it’s brought to you by ICS.</p>} />,
-  ]} />),
+  new RouteObject('/', () => <Homepage />),
   new RouteObject('/pdp', () => <PDP />),
   new RouteObject('/plp', () => <PLP/>),
   new RouteObject('/category-content', () => <CategoryContent />),
